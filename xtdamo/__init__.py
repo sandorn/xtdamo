06f08109d1cea3e13d33a2f476358f91dc624982
sandorn <sandorn@live.cn>
2025-10-20 20:44:29 +0800

    v0.1.1

--- xtdamo/__init__.py
+++ xtdamo/__init__.py
@@ -13,21 +13,14 @@ Github       : https://github.com/sandorn/home
 
 from __future__ import annotations
 
-__name__ = 'xtdamo'
-__version__ = '0.1.0'
-
 from .config import Config
 from .damo import DmExcute
 from .dependencies import CRYPTO_AVAILABLE, WIN32_AVAILABLE, WIN32GUI_AVAILABLE, DependencyChecker, check_dependency, get_available_dependencies, get_missing_dependencies
 from .enum_wind import get_windows_by_criteria
 from .secure_config import DmCredentials, dm_credentials
 
-# 为了向后兼容，保留 DM 别名
-DM = DmExcute
-
 __all__ = (
     'CRYPTO_AVAILABLE',
-    'DM',  # 向后兼容
     'WIN32GUI_AVAILABLE',
     'WIN32_AVAILABLE',
     'Config',
@@ -40,6 +33,4 @@ __all__ = (
     'get_available_dependencies',
     'get_missing_dependencies',
     'get_windows_by_criteria',
-    # 版本信息
-    '__version__',
 )
